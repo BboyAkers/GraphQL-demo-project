8204b575bcba1ee869dd83bee29abfb6b2bfc633
Austin Akers <austin.akers5@gmail.com>
2023-12-07 06:27:16 -0800

    scaffold for project to demo graphql queries and mutations completed

---
+++ start/schemas/schema.js
@@ -0,0 +1,18 @@
+// import * as _ from 'lodash'
+// import axios from 'axios';
+import { GraphQLSchema } from 'graphql';
+
+// CompanyType
+
+// UserType
+
+// RootQueryType
+
+// RootMutation
+
+const rootSchema = new GraphQLSchema({
+  // query
+  // mutation
+});
+
+export { rootSchema }; 
